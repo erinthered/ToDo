db5fd6c6c707c2fbbe2d83a97e5c6bc75f47ee8d
erinthered <erinbesswilliams@gmail.com>
2018-09-03 16:58:14 -0400

    Add Sign Up template

--- todo/app.py
+++ todo/app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
