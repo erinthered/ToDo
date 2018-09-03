9451d87343e939fdbde85d07ab76a9a119c54ae2
erinthered <erinbesswilliams@gmail.com>
2018-09-03 18:41:05 -0400

    Add database, user table

---
+++ todo/tabledef.py
@@ -0,0 +1,21 @@
+from sqlalchemy import *
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+engine = create_engine('sqlite:///todo.db', echo=True)
+
+# Base class
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+
+    def __init__(self, username):
+        self.username = username
+
+Base.metadata.creat_all(engine)
