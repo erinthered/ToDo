6483dac3d1800fbdcd9f1d7e49ded25309e8d612
erinthered <erinbesswilliams@gmail.com>
2018-09-03 18:23:05 -0400

    Add signup functionality, still need db

--- todo/app.py
+++ todo/app.py
@@ -1,7 +1,21 @@
 from flask import Flask
 from flask import render_template
+from wtforms import Form, TextField, validators, StringField, SubmitField
+# Used by Userform.signup
+from flask import session, flash, request, redirect, abort
+from sqlalchemy.orm import sessionmaker
+import os
 
 app = Flask(__name__)
+
+class UserForm(Form):
+    username = StringField('Username', validators=[validators.required()])
+
+    # Protects against CSRF attacks (WTForms)
+    def reset(self):
+        blankData = MultiDict([ ('csrf', self.reset_crsf()) ])
+        self.process(blankData)
+
 
 @app.route('/')
 @app.route('/index')
@@ -10,4 +24,26 @@ def index():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    form = UserForm(request.form)
+    # If user has sent pressed submit button
+    if request.method == 'POST':
+        # Get username from form
+        username = request.form['username']
+        if form.validate():
+            # Start DB session
+            Session = sessionmaker(bind=engine)
+            s = Session()
+            # Add user to table
+            user = User(username)
+            s.add(user)
+            # Commit changes to DB
+            s.commit
+
+            flash('Hello ' + username + '! You are now registered!')
+    else:
+        flash('Error: Username Required.')
+
+    return render_template('signup.html', form=form)
+
+
+app.secret_key = os.urandom(12)
